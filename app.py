ec879883e51ee2e7c03e7b14cb34f7a13166653a
RondoChen <rondochen@163.com>
2021-11-09 18:03:11 +0800

    Add files via upload
    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import sys
+import time
+import configparser
+import redis
+
+from flask import Flask
+
+cf=configparser.ConfigParser()
+try:
+    cf.read('app.ini', encoding='UTF-8')
+except:
+    print('missing configuration file: app.ini')
+    exit()
+
+app = Flask(__name__)
+cache = redis.Redis(host=cf.get('redis','redis_host'), port=cf.get('redis','redis_port'), db=cf.get('redis','redis_db'), password=cf.get('redis','redis_password'))
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+
+if __name__ == "__main__":
+    app.run()
